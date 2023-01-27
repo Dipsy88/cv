464c929783762d674497f54504ba20f894c7e85b
dipeshpradhan <dipesh.pradhan@nav.no>
2023-01-27 10:55:17 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function App() {
+    // define theme
+    const theme = createTheme({
+        palette: {
+            primary: {
+                light: "#63b8ff",
+                main: "#0989e3",
+                dark: "#005db0",
+                contrastText: "#000",
+            },
+            secondary: {
+                main: "#4db6ac",
+                light: "#82e9de",
+                dark: "#00867d",
+                contrastText: "#000",
+            },
+        },
+    });
+  return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box
+            height="100vh"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+        >
+          <Paper
+              elevation={3}
+              sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+          >
+            <Typography color="primary.dark" variant="h1">
+              Starter App
+            </Typography>
+          </Paper>
+        </Box>
+      </ThemeProvider>
+  );
+}
+
+export default App;
